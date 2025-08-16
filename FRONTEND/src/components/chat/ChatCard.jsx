146178d5ba5e153263e2c080ad83cfe6bfd3609a
RaviKumarYadav15 <ravikumaryaadav1511@gmail.com>
2025-08-16 14:33:07 +0530

    remove unused import of useRef in ChatCard component

--- FRONTEND/src/components/chat/ChatCard.jsx
+++ FRONTEND/src/components/chat/ChatCard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectChat } from '../../features/chat/chatSlice';
 
